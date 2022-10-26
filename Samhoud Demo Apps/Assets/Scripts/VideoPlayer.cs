ffa9bac16151ba1f49e6fa0a007a403470e30e27
LV <viveiros.lucas@hotmail.com>
2022-10-26 13:37:32 +0100

    Update VideoPlayer.cs

--- Samhoud Demo Apps/Assets/Scripts/VideoPlayer.cs
+++ Samhoud Demo Apps/Assets/Scripts/VideoPlayer.cs
@@ -6,20 +6,17 @@ public class VideoPlayer : MonoBehaviour
 {
     public UnityEngine.Video.VideoClip videoClip;
 
-    // Start is called before the first frame update
     void Start()
     {
         var videoPlayer = gameObject.AddComponent<UnityEngine.Video.VideoPlayer>();
-        //var audioSource = gameObject.AddComponent<AudioSource>();
-
-
+        var audioSource = gameObject.AddComponent<AudioSource>();
         videoPlayer.playOnAwake = false;
         videoPlayer.clip = videoClip;
-        //videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.MaterialOverride;
-       // videoPlayer.targetMaterialRenderer = GetComponent<Renderer>();
-        //videoPlayer.targetMaterialProperty = "_MainTex";
-        //videoPlayer.audioOutputMode = UnityEngine.Video.VideoAudioOutputMode.AudioSource;
-        //videoPlayer.SetTargetAudioSource(0, audioSource);
+        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.MaterialOverride;
+        videoPlayer.targetMaterialRenderer = GetComponent<Renderer>();
+        videoPlayer.targetMaterialProperty = "_MainTex";
+        videoPlayer.audioOutputMode = UnityEngine.Video.VideoAudioOutputMode.AudioSource;
+        videoPlayer.SetTargetAudioSource(0, audioSource);
     }
 
     void setPlayVideo() 
